ed32c325e664d8d94d54aa7a833a1df320a873f4
Gbikex <csatgabor@gmail.com>
2026-03-29 17:21:23 +0200

    Add: App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import TaskMain from "./assets/pages/TaskMain";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello</h1>
+      <TaskMain />
     </>
   );
 }
