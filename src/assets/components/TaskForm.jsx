755d356226f15de86d9df083046795aa66333df4
Gbikex <csatgabor@gmail.com>
2026-03-29 11:32:40 +0200

    Add: base state logic

--- src/assets/components/TaskForm.jsx
+++ src/assets/components/TaskForm.jsx
@@ -1,22 +1,82 @@
+import { useReducer } from "react";
 import styles from "./TaskForm.module.css";
 
+const InitialState = {
+  task: [],
+  titleInput: "",
+  descriptionInput: "",
+  statusInput: "",
+  priorityInput: "",
+  dueDateInput: "",
+};
+
+console.log(InitialState);
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "setTitleInput":
+      return { ...state, titleInput: action.payLoad };
+    case "setDescriptionInput":
+      return { ...state, descriptionInput: action.payLoad };
+    case "setStatusInput":
+      return { ...state, statusInput: action.payLoad };
+    case "setPriorityInput":
+      return { ...state, priorityInput: action.payLoad };
+    case "setDueDateInput":
+      return { ...state, dueDateInput: action.payLoad };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
 function TaskForm() {
+  const [
+    { titleInput, descriptionInput, statusInput, priorityInput, dueDateInput },
+    dispatch,
+  ] = useReducer(reducer, InitialState);
+
+  const handleTitleInput = function (e) {
+    dispatch({ type: "setTitleInput", payLoad: e.target.value });
+  };
+
+  const handleDescriptionInput = function (e) {
+    dispatch({ type: "setDescriptionInput", payLoad: e.target.value });
+  };
+
+  const handleStatusInput = function (e) {
+    dispatch({ type: "setStatusInput", payLoad: e.target.value });
+  };
+
+  const handlePriorityInput = function (e) {
+    dispatch({ type: "setPriorityInput", payLoad: e.target.value });
+  };
+
+  const handleDueDateInput = function (e) {
+    dispatch({ type: "setDueDateInput", payLoad: e.target.value });
+  };
+
   return (
     <div>
       <div className={styles.divForm}>
-        <p>Task Details</p>
+        <h1>Task Details</h1>
         <form>
           <p>Title</p>
-          <input></input>
+          <input value={titleInput} onChange={handleTitleInput}></input>
           <p>Description</p>
-          <input></input>
+          <input
+            value={descriptionInput}
+            onChange={handleDescriptionInput}
+          ></input>
           <p>Status</p>
-          <input></input>
+          <input value={statusInput} onChange={handleStatusInput}></input>
           <p>Priority</p>
-          <input></input>
+          <input value={priorityInput} onChange={handlePriorityInput}></input>
           <p>Due Date</p>
-          <input></input>
+          <input value={dueDateInput} onChange={handleDueDateInput}></input>
         </form>
+        <>
+          <button>Add</button>
+        </>
       </div>
     </div>
   );
